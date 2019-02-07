5be3fa020cb47e36a664972d9108076175710b92
Herman <hrivas5726@aol.com>
2019-02-07 23:43:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+    array = programmer_hash[:dennis_ritchie][:languages]
+    array.join
 end
 
 def adding_matz
